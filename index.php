0d437be46cacf85edee3183a5f03c8da4a044c87
John Turingan <webmaster@scriptsandpixels.com>
2014-08-19 20:22:10 +0800

    add resources

---
+++ index.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="HandheldFriendly" content="True" />
+	<meta name=apple-mobile-web-app-capable content=yes>
+	<meta name=apple-mobile-web-app-status-bar-style content=black>
+	<meta name="viewport" content="initial-scale=1, maximum-scale=1"/>
+
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/fontawesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/main.css">
+
+	<title>HSBC iGo</title>
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			
+		</header>
+		<div class="content">
+			
+		</div>
+		<footer>
+			
+		</footer>
+	</div>
+</body>
+</html>
+
