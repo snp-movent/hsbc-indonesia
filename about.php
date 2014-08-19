fc4bfe71f62f30cdd7b203d40fce185e6984153a
developer-snp <developer.snp@gmail.com>
2014-08-20 01:06:40 +0800

    about php and css

---
+++ about.php
@@ -0,0 +1,20 @@
+<div class="snp-about">
+	<div class="about-hold">
+		<h3><span>Tentang </span>Buddy Meter</h3>
+	</div>
+	<div class="about-text">
+		<p class="text-bold">
+			Sesuai dengan tagline “Together We Advance”, Buddy Meter adalah salah satu wujud nyata dimana HSBC Advance memahami gaya hidup dan kebutuhan dari nasabahnya.
+		</p>
+		<p>
+			Setiap ambisi dan impian individu pasti mendapat dukungan dari orang terdekatnya, seperti inspirasi dari jaringan keluarga, teman dan kolega untuk meraih ambisi dan impian yang lebih tinggi
+		</p>
+		<p>
+			Jadi, apapun ambisi dan impian Anda untuk masa depan, mereka lebih mungkin decapai dengan buntuan orang-orang di sekeliling Anda.  
+		</p>
+		<p>
+			Dengan Buddy Meter, Anda dan orang-orang terdekat Anda dapat mendukung satu sama lain untuk mendapatkan cash back dan suku bunga menarik. HSBC Advance pun hadir sebagai salah satu pendukung untuk bersama-sama mewujudkan kisah keberhasilan Anda. 
+
+		</p>
+	</div>
+</div>
