a273a19eea35b3af8f6ed261b106f644e6e25496
developer-snp <developer.snp@gmail.com>
2014-08-20 02:21:54 +0800

    new and revised

---
+++ contact-us.php
@@ -0,0 +1,10 @@
+<div class="snp-contact">
+	<div class="contact-hold">
+		<h3>Hubungi Kami</h3>
+	</div>
+	<div class="contact-content">
+		<h4>Untuk pertanyaan lebih lanjut, mohon hubungi kami di:</h4>
+		<h3><span class="fa fa-phone"></span> (021)52914722 </h3>
+		<p class="small-text">atau kunjungi cabang HSBC terdekat Anda.</p>
+	</div>
+</div>
