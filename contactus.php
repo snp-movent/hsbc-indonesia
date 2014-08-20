358a72acc0cb28a49959f8e1f560c3e46d8cdd16
John Turingan <webmaster@scriptsandpixels.com>
2014-08-21 07:16:29 +0800

    final html files

--- contactus.php
+++
@@ -1,7 +0,0 @@
-<div class="snp-contact">
-	<div class="contact-content">
-		<h4>Untuk pertanyaan lebih lanjut, mohon hubungi kami di:</h4>
-		<h3><span class="fa fa-phone"></span> (021)52914722 </h3>
-		<p class="small-text">atau kunjungi cabang HSBC terdekat Anda.</p>
-	</div>
-</div>
