506cf2c7f269c368d5bee9a0b6b3f5e679965633
developer-snp <developer.snp@gmail.com>
2014-08-20 11:02:21 +0800

    thankyou php and css

---
+++ thankyou.php
@@ -0,0 +1,24 @@
+<div class="snp-thankyou">
+	<div class="thankyou-hold">
+        <h4>Anda telah terdaftar dalam promosi HSBC Advance.</h4>
+    </div>
+    <div class="thankyou-content">
+    	<h4 class="thankyou-text">Selain cash back dan suku bunga menarik, HSBC Advance menawarkan berbagai macam keistimewaan untuk nasabahnya:</h4>
+    	<div class="thankyou-text">
+	     	<a href="#"><span class="fa fa-angle-right"></span>Akses Internet Banking dan Mobile Banking 24/7 untuk kemudahan mengelola rekening Anda dan melakukan transaksi.</a>
+  			
+  		</div>
+  		<div class="thankyou-text">
+  			<a href="#"><span class="fa fa-angle-right"></span>Monitor pengelolaan keuangan dengan beragam notifikasi perbankan, langsung ke ponsel Anda.</a>
+  		</div>	
+  		<div class="thankyou-text">
+  			<a href="#"><span class="fa fa-angle-right"></span>Beragam pilihan solusi keuangan terpadu, untuk berbagai kebutuhan lifestyle, membeli rumah, mengembangkan dan melindungi kekayaan Anda.
+  			</a>
+  		</div>	
+  		<div class="thankyou-text">
+  			<a href="#"><span class="fa fa-angle-right"></span>arif dan harga bersaing untuk pembelian solusi Wealth Management melalui Personal Internet Banking.
+  			</a>
+  		</div>
+  		<p class="thankyou-text">Ajak keluarga dan teman-teman Anda untuk bergabung bersama HSBC Advance agar mereka dapat juga menikmati berbagai keistimewaan tersebut di atas.</p>
+  	</div>
+</div>
