cfa4f78abf2697587f90b161b58c0d124e327bad
snp-movent <developer.snp@gmail.com>
2014-08-20 23:02:25 +0800

    home

--- home.php
+++ home.php
@@ -1,1 +1,9 @@
-isda
+<div class="snp-comingsoon">
+    <div class="section-title">
+        <p><strong>Nikmati suku bunga 7,5% dan <em>cash back</em> hingga Rp300.000</strong></p>
+        <p>Dengan penempatkan deposito Rp50 juta selama 6 bulan sekarang juga.</p>
+    </div>
+    <div class="section-content">
+        
+    </div>
+</div>
