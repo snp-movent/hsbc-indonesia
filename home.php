c3303cff282a471479181ed4f85162242aabdd5b
John Turingan <webmaster@scriptsandpixels.com>
2014-08-21 02:37:15 +0800

    message

--- home.php
+++ home.php
@@ -1,9 +1,134 @@
-<div class="snp-comingsoon">
-    <div class="section-title">
-        <p><strong>Nikmati suku bunga 7,5% dan <em>cash back</em> hingga Rp300.000</strong></p>
-        <p>Dengan penempatkan deposito Rp50 juta selama 6 bulan sekarang juga.</p>
-    </div>
-    <div class="section-content">
-        
-    </div>
-</div>
+	<!DOCTYPE html>
+<html>
+<head>
+	<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="HandheldFriendly" content="True" />
+	<meta name=apple-mobile-web-app-capable content=yes>
+	<meta name=apple-mobile-web-app-status-bar-style content=black>
+	<meta name="viewport" content="initial-scale=1, maximum-scale=1"/>
+
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/fontawesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/font/fonts.css">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/pages.css">
+
+	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script src="js/uni.script.js"></script>
+	<title>HSBC iGo</title>
+</head>
+<body>
+	<div class="wrapper">
+		<header class="snp-header homepage">
+			<div class="logo-hold">
+				<div class="socialmedia-hold">
+					<span>
+						<a href="#"><i class="fa fa-facebook"></i></a>
+						<a href="#"><i class="fa fa-twitter"></i></a>
+						<a href="#"><i class="fa fa-linkedin"></i></a>
+						<a href="#"><i class="fa fa-pinterest"></i></a>
+					</span>
+				</div>
+			</div>
+			<nav class="snp-navigation">
+				<a href="#" id="snp-navbtn"><i class="fa fa-bars"></i>&nbsp;</a>
+				<span>Menu</span>
+				<ul id="snp-navblk" class="blk-hidden">
+					<li><a href="#"><span>Tentang</span> Buddy Meter</a></li>
+					<li><a href="#"><span>Cara Kerja</span> Buddy Meter</a></li>
+					<li><a href="#"><span>Manfaat Nasabah</span> HSBC Advance</a></li>
+					<li><a href="#"><span>FAQ</span> Buddy Meter</a></li>
+					<li><a href="#"><span>Hubungi</span> Kami</a></li>
+				</ul>
+			</nav>
+
+			<a href="" class="hpageBtn">Daftar Individu</a>
+			<a href="" class="hpageBtn2 ">Daftar Grup*</a>
+			
+		</header>
+		<section class="content">
+			
+			<div class="hmpagetop">
+				<h1>Nikmati suku bunga 7,5% dan cash back hingga Rp300.000</h1>
+				<p>Dengan penempatkan deposito Rp50 juta selama 6 bulan sekarang juga.</p>
+			</div>
+
+			<div class="thermobox">
+				<p><strong>170 orang</strong> sudah terdaftar dengan cash back:</p>
+				<h1>Rp180.000</h1>
+
+				<div class="thermo">
+					<div>
+						<div class="levelbox">
+							<p class="lvltop"><span>300</span> <span>300K</span></p>
+							<p class="lvl2nd"><span>250</span> <span>250K</span></p>
+							<p class="lvl3rd"><span>200</span> <span>200K</span></p>
+							<p class="lvl4th"><span>150</span> <span>150K</span></p>
+							<p class="lvl5th"><span>100</span> <span>100K</span></p>
+							<p class="lvl6th"><span>50</span> <span>50K</span></p>
+						</div>
+						<div class="level">
+							<span style="top:30%">140</span>
+						</div>
+					</div>
+					<p class="lvlabel">
+						<span>JUMLAH PENDAFTAR</span>
+						<span>JUMLAH CASHBACK</span>
+					</p>
+					<p class="bagaima">
+						<a href=""><i class="fa fa-info-circle"></i>Bagaimana cara kerja Buddy Meter</a>
+					</p>
+
+				</div>
+
+
+			</div>
+			<div class="calbox">
+				<h1>Penawaran terbatas ini akan ditutup dalam:</h1>
+				<p>
+					8
+					<small>
+					Hari
+					</small>
+				</p>
+				<p>
+					8
+					<small>
+					Jam
+					</small>
+				</p>
+				<p>
+					8
+					<small>
+					Menit
+					</small>
+				</p>
+				<p>
+					8
+					<small>
+					Detik
+					</small>
+				</p>
+				<a href="" class="hpageBtn">Daftar Individu</a>
+				<hr/>
+				<a href="" class="hpageBtn2 ">Daftar Grup*</a>
+				<span>*Dapatkan tambahan cash back Rp1,2 juta khusus untuk Anda dan teman/keluarga dengan mendaftar sebagai grup</span>
+				<hr/>
+				<span class="bagaima">
+					<a href=""><i class="fa fa-info-circle"></i>Bagaimana proses pendaftaran pribadi maupun grup?</a>
+				</span>
+			</div>
+
+		</section>
+		<footer class="snp-footer">
+			<p>&copy; Copyright HSBC Bank Indonesia <?php echo date('Y') ?> . ALL RIGHTS RESERVED</p>
+			<p>
+				Our privacy policy explains how we treat your personal data and protect your privacy when you use our Services. By using our Services, you agree that Google can use such data in accordance with our privacy policy. You will ensure that at all times you use the Services, the Properties have a clearly labeled and easily accessible privacy policy that provides end users with clear and comprehensive information about cookies, device-specific information, location information and other information stored on, accessed on, or collected from end users’ devices in connection with the Services, including, as applicable, information about end users’ options for cookie management.  You will use commercially reasonable efforts to ensure that an end user gives consent to the storing and accessing of cookies, device-specific information, location information or other information on the end user's device in connection with the Services where such consent is required by law.
+			</p>
+		</footer>
+	</div>
+</body>
+</html>
+
