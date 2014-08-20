094ea4567f65a83996fa7353fb771a85cf430bb8
snp-movent <developer.snp@gmail.com>
2014-08-20 19:23:20 +0800

    added comingsoon html

---
+++ comingsoon.php
@@ -0,0 +1,15 @@
+<div class="snp-comingsoon">
+    <div class="section-title">
+        <p>Saat ini periode promosi sedang berhenti. Silakan coba datang lagi lain waktu</p>
+    </div>
+    <div class="section-content">
+        <div class="text-hold">
+            <p><strong>Nikmati suku bunga 7,5% dan <em>cash back</em> hingga Rp300.000</strong></p>
+            <p>Dengan penempatkan deposito Rp50 juta selama 6 bulan sekarang juga.</p>
+        </div>
+        <div class="pic-hold">
+            <img src="/images/pages/barcomingsoon.jpg" alt="">
+            <img src="/images/pages/remainingtime.jpg" alt="">
+        </div>
+    </div>
+</div>
